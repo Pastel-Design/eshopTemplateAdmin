143aec2f7e753825882593b629fdbc717dcdd213
kukivac <50522557+kukivac@users.noreply.github.com>
2021-01-18 15:47:27 +0100

    Update tabs.js

--- www/scripts/tabs.js
+++ www/scripts/tabs.js
@@ -1,1 +1,21 @@
-let tabGroupNames=[];document.querySelectorAll(".tab-content").forEach(a=>{let b=a.getAttribute("tab-group");tabGroupNames.includes(b)||tabGroupNames.push(b)}),tabGroupNames.forEach(a=>{let b=document.querySelectorAll(".tab-content[tab-group='"+a+"']");b.forEach(a=>{a.classList.add("hidden")}),b[0].classList.remove("hidden")}),document.querySelectorAll(".tab-links").forEach(a=>{a.addEventListener("click",()=>{opentab(a.getAttribute("tab-controls"))})});function opentab(a){let b=document.querySelector("#"+a);document.querySelectorAll(".tab-content[tab-group='"+b.getAttribute("tab-group")+"']").forEach(a=>{a.classList.add("hidden")}),b.classList.remove("hidden")}
+let tabGroupNames = [];
+document.querySelectorAll(".tab-content").forEach(a => {
+    let b = a.getAttribute("tab-group");
+    tabGroupNames.includes(b) || tabGroupNames.push(b)
+}), tabGroupNames.forEach(a => {
+    let b = document.querySelectorAll(".tab-content[tab-group='" + a + "']");
+    b.forEach(a => {
+        a.classList.add("hidden")
+    }), b[0].classList.remove("hidden")
+}), document.querySelectorAll(".tab-links").forEach(a => {
+    a.addEventListener("click", () => {
+        opentab(a.getAttribute("tab-controls"))
+    })
+});
+
+function opentab(a) {
+    let b = document.querySelector("#" + a);
+    document.querySelectorAll(".tab-content[tab-group='" + b.getAttribute("tab-group") + "']").forEach(a => {
+        a.classList.add("hidden")
+    }), b.classList.remove("hidden")
+}
